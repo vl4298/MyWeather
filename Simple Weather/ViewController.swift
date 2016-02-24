6bf318c70b24994d0c268ef619ed471dabdf54f4
Van Luu <valu4298@gmail.com>
2016-02-24 16:47:20 +0700

    Initial Commit

---
+++ Simple Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Weather
+//
+//  Created by Van Luu on 24/02/2016.
+//  Copyright © 2016 Van Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
