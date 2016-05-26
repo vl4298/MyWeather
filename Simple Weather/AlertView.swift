35e0e33df1fdb6c4ffa5001e658b288bd4b81191
Van Luu <valu4298@gmail.com>
2016-05-26 20:39:29 +0700

    first commit

---
+++ Simple Weather/AlertView.swift
@@ -0,0 +1,13 @@
+//
+//  AlertLabel.swift
+//  Simple Weather
+//
+//  Created by Van Luu on 27/02/2016.
+//  Copyright © 2016 Van Luu. All rights reserved.
+//
+
+import UIKit
+
+class AlertView: UIView {
+
+}
---
+++ Simple Weather/WXDailyForecastCollection.swift
@@ -0,0 +1,13 @@
+//
+//  WXDailyForecastCollection.swift
+//  Simple Weather
+//
+//  Created by Van Luu on 24/02/2016.
+//  Copyright © 2016 Van Luu. All rights reserved.
+//
+
+import UIKit
+
+class WXDailyForecastCollection: NSObject {
+
+}
